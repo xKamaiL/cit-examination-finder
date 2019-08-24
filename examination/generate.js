3492e95f1c0c5244b25c028721eaf1d0b4a952e4
xKamaiL <xkamail@icloud.com>
2019-08-24 23:25:33 +0700

    added firebase hosting deploy to serve exam.json

--- examination/generate.js
+++ examination/generate.js
@@ -3,61 +3,67 @@ const fs = require('fs');
 const _ = require('lodash');
 console.log('Starting...');
 
-const subject = {
-  392131: 'Physic I',
-  392132: 'Physic II',
-  392133: 'Physic III',
-  392134: 'Physic IV',
-  392135: 'Physic V',
-  392136: 'Physic VI',
-  392151: 'Chemistry I',
-  392152: 'Chemistry II',
-  392153: 'Chemistry III',
-  392154: 'Chemistry IV',
-  392155: 'Chemistry V',
-  392156: 'Chemistry VI',
-  394171: 'Mathematics I',
-  394172: 'Mathematics II',
-  394173: 'Mathematics III',
-  394174: 'Mathematics IV',
-  394175: 'Mathematics V',
-  394176: 'Mathematics VI',
-  393141: 'English I',
-  393142: 'English II',
-  393143: 'English III',
-  393144: 'English IV',
-  393145: 'English V',
-  393146: 'English VI',
-  393161: 'Thai I',
-  393162: 'Thai II',
-  393163: 'Thai III',
-  395181: 'Social Study I',
-  395182: 'Social Study II',
-  395183: 'Social Study III',
-  396121: 'Physical Education',
-  340151: 'Electrical Materials',
-  340152: 'Electrical Measuring Instruments',
-  341151: 'Electric Circuits I',
-  341152: 'Electric Circuits II',
-  353152: 'Electronic Circuit I',
-  353153: 'Electronic Circuit II',
-  353156: 'Microprocessor',
-  353155: 'Communication Technology'
-};
+const subject = [
+  { id: '340151', name: 'Electrical Materials' },
+  { id: '340152', name: 'Electrical Measuring Instruments' },
+  { id: '341151', name: 'Electric Circuits I' },
+  { id: '341152', name: 'Electric Circuits II' },
+  { id: '353152', name: 'Electronic Circuit I' },
+  { id: '353153', name: 'Electronic Circuit II' },
+  { id: '353155', name: 'Communication Technology' },
+  { id: '353156', name: 'Microprocessor' },
+  { id: '392131', name: 'Physic I' },
+  { id: '392132', name: 'Physic II' },
+  { id: '392133', name: 'Physic III' },
+  { id: '392134', name: 'Physic IV' },
+  { id: '392135', name: 'Physic V' },
+  { id: '392136', name: 'Physic VI' },
+  { id: '392151', name: 'Chemistry I' },
+  { id: '392152', name: 'Chemistry II' },
+  { id: '392153', name: 'Chemistry III' },
+  { id: '392154', name: 'Chemistry IV' },
+  { id: '392155', name: 'Chemistry V' },
+  { id: '392156', name: 'Chemistry VI' },
+  { id: '393141', name: 'English I' },
+  { id: '393142', name: 'English II' },
+  { id: '393143', name: 'English III' },
+  { id: '393144', name: 'English IV' },
+  { id: '393145', name: 'English V' },
+  { id: '393146', name: 'English VI' },
+  { id: '393161', name: 'Thai I' },
+  { id: '393162', name: 'Thai II' },
+  { id: '393163', name: 'Thai III' },
+  { id: '394171', name: 'Mathematics I' },
+  { id: '394172', name: 'Mathematics II' },
+  { id: '394173', name: 'Mathematics III' },
+  { id: '394174', name: 'Mathematics IV' },
+  { id: '394175', name: 'Mathematics V' },
+  { id: '394176', name: 'Mathematics VI' },
+  { id: '395181', name: 'Social Study I' },
+  { id: '395182', name: 'Social Study II' },
+  { id: '395183', name: 'Social Study III' },
+  { id: '396121', name: 'Physical Education' }
+];
 
-let files = [];
-let filesP1 = {
-  midterm: [],
-  final: []
-};
-let filesP2 = {
-  midterm: [],
-  final: []
-};
-let fileP3 = {
-  midterm: [],
-  final: []
-};
+function getFileName(name) {
+  let result = _.find(subject, {
+    id: name
+  });
+  if (result) {
+    return result.name;
+  } else {
+    return name;
+  }
+}
+
+let term1 = {
+    midterm: [],
+    final: []
+  },
+  term2 = {
+    midterm: [],
+    final: []
+  };
 const convertToArray = file => {
   if (file.type === 'folder') {
     file.items.map(a => {
@@ -68,7 +74,7 @@ const convertToArray = file => {
     let year = 'Unknown';
     let fileName = 'Unknown';
     year = file.path.split('/')[1];
-    fileName = `${file.name.replace('.pdf', '')} (${year}) `;
+    fileName = `${getFileName(file.name.replace('.pdf', ''))} (${year}) `;
     let isTerm1 = file.path.indexOf('term1') > -1 ? true : false;
     let isFinal = file.path.indexOf('midterm') > -1 ? true : false;
     console.log(
@@ -76,6 +82,17 @@ const convertToArray = file => {
         fileName +
         `${isFinal ? 'Final' : 'Midterm'} ${isTerm1 ? 'TERM1' : 'TERM2'}`
     );
+    if (isTerm1) {
+      term1[isFinal ? 'final' : 'midterm'].push({
+        ...file,
+        fileName
+      });
+    } else {
+      term2[isFinal ? 'final' : 'midterm'].push({
+        ...file,
+        fileName
+      });
+    }
   }
 };
 return axios.default
@@ -83,8 +100,11 @@ return axios.default
   .then(({ data }) => {
     convertToArray(data);
     return fs.writeFile(
-      'examination.json',
-      JSON.stringify(data),
+      'public/examination.json',
+      JSON.stringify({
+        term1,
+        term2
+      }),
       'utf8',
       function(err) {
         if (err) {
@@ -102,7 +122,6 @@ return axios.default
   });
 
 /*
-
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
